72f26f29a2310e26a8cffacf291fd4fd6621b57e
chathura9716 <lakmalc97516@gmail.com>
2022-09-16 15:06:04 +0530

    flutter try

---
+++ lib/screens/ticket_screen.dart
@@ -0,0 +1,173 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:gap/gap.dart';
+import 'package:ticketbookingapp/screens/ticket_view.dart';
+import 'package:ticketbookingapp/utils/app_layout.dart';
+import 'package:ticketbookingapp/utils/app_styles.dart';
+import 'package:ticketbookingapp/widgets/column_layout.dart';
+import 'package:ticketbookingapp/widgets/ticket_tabs.dart';
+import 'package:barcode_widget/barcode_widget.dart';
+
+import '../utils/app_info_list.dart';
+import '../widgets/layout_builder_widget.dart';
+
+class TicketScreen extends StatelessWidget {
+  const TicketScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size =AppLayout.getSize(context);
+
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body:Stack(
+        children: [ ListView(
+          padding: EdgeInsets.symmetric(horizontal: AppLayout.getHeight(20),vertical: AppLayout.getHeight(20)),
+          children: [
+            Gap((AppLayout.getHeight(40))),
+            Text('Tickets',style: Styles.headLineStyle1,),
+            Gap((AppLayout.getHeight(20))),
+
+            const AppTicketTabs(firstTab: 'Upcoming', secondTab: 'previous'),
+            Gap((AppLayout.getHeight(20))),
+            Container(
+              padding: EdgeInsets.only(left: AppLayout.getHeight(15)),
+              child: TicketView(ticket: ticketList[0],isColor: true,),
+            ),
+            const SizedBox(height:1),
+            Container(
+
+              padding: EdgeInsets.symmetric(horizontal: 15,vertical: 20),
+              margin: EdgeInsets.symmetric(horizontal: 15,),
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      AppColumnLayout(firstText: 'Flutter DB', secondText: 'Passenger',alignment: CrossAxisAlignment.start,isColor: false,),
+                      AppColumnLayout(firstText: '24354353242', secondText: 'Passport',alignment: CrossAxisAlignment.end,isColor: false,),
+
+                    ],
+                  ),
+                  Gap((AppLayout.getHeight(20))),
+
+                  const AppLayoutBuilderWidget(section:15 ,isColor: false,width: 5,),
+                  Gap((AppLayout.getHeight(20))),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      AppColumnLayout(firstText: '42546 545 48547', secondText: 'Number of E-ticket',alignment: CrossAxisAlignment.start,isColor: false,),
+                      AppColumnLayout(firstText: 'B2SG28', secondText: 'Booking code',alignment: CrossAxisAlignment.end,isColor: false,),
+
+                    ],
+                  ),
+                  Gap((AppLayout.getHeight(20))),
+                  const AppLayoutBuilderWidget(section:15 ,isColor: false,width: 5,),
+                  Gap((AppLayout.getHeight(20))),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Image.asset("assets/images/visa.png",scale:11,),
+                              Text("*** 2435",style: Styles.headLineStyle3,),
+                            ],
+
+                          ),
+                          Gap(5),
+                          Text("Payment method",style: Styles.headLineStyle4,)
+                        ],
+                      ),
+                      AppColumnLayout(firstText: '\$234.99', secondText: 'Price',alignment: CrossAxisAlignment.end,isColor: false,),
+
+
+                    ],
+                  ),
+                  const SizedBox(height:1),
+
+
+                ],
+              ),
+            ),
+            /*
+            bar code
+             */
+            const SizedBox(height:1),
+           Container(
+
+             decoration: BoxDecoration(
+                 color: Colors.white,
+                 borderRadius: BorderRadius.only(
+                 bottomRight:Radius.circular(AppLayout.getHeight(21)),
+                 bottomLeft: Radius.circular(AppLayout.getHeight(21)),
+               )
+             ),
+             margin: EdgeInsets.only(left: AppLayout.getHeight(15),right: AppLayout.getHeight(15)),
+             padding: EdgeInsets.only(top: AppLayout.getHeight(15),bottom: AppLayout.getHeight(15)),
+             child: Container(
+               padding: EdgeInsets.symmetric(horizontal: AppLayout.getHeight(15)),
+               child: ClipRRect(
+                 borderRadius:BorderRadius.circular(AppLayout.getHeight(15)),
+                 child: BarcodeWidget(
+                   barcode:Barcode.code128(),
+                   data:'https://github.com/martinovovo',
+                   drawText:false,
+                   color:Styles.textColor,
+                   width:double.infinity,
+                   height:70
+                 ),
+               ),
+             ),
+           ),
+            Gap((AppLayout.getHeight(20))),
+            Container(
+              padding: EdgeInsets.only(left: AppLayout.getHeight(15)),
+              child: TicketView(ticket: ticketList[0],),
+            ),
+            
+          ],
+        ),
+          Positioned(
+            left: AppLayout.getHeight(21),
+            top: 295,
+            child: Container(
+              padding: EdgeInsets.all(3),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Styles.textColor,width: 2)
+              ),
+              child: CircleAvatar(
+                maxRadius: 4,
+                backgroundColor: Styles.textColor,
+              ),
+            ),
+          ),
+          Positioned(
+            right: AppLayout.getHeight(21),
+            top: 295,
+            child: Container(
+              padding: EdgeInsets.all(3),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Styles.textColor,width: 2)
+              ),
+              child: CircleAvatar(
+                maxRadius: 4,
+                backgroundColor: Styles.textColor,
+              ),
+            ),
+          )
+
+        ]
+      ) ,
+    );
+  }
+}
+
+
